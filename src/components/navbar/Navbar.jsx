df81a30401f52ec751c60cfc6b2a9075a3884a64
Shr4528 <Shr4528@gmail.com>
2023-10-14 17:59:32 -0500

    icons

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,32 @@
+import './navbar.scss';
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      {/* sidebar */}
+      <div className='wrapper'>
+        <span>X Z</span>
+        <div className='social'>
+          <a href='#'>
+            {' '}
+            <img src='/facebook.png' alt='' />
+          </a>
+          <a href='#'>
+            {' '}
+            <img src='/instagram.png' alt='' />
+          </a>
+          <a href='#'>
+            {' '}
+            <img src='/youtube.png' alt='' />
+          </a>
+          <a href='#'>
+            {' '}
+            <img src='/dribbble.png' alt='' />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;

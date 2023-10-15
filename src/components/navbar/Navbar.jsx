b21b27dca22166736f2b906cfb0bcbb0303ddbe5
Shr4528 <Shr4528@gmail.com>
2023-10-15 17:53:44 -0500

    hero

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
           initial={{ opacity: 0, scale: 0.5 }}
           animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 0.5 }}>
-          X Z
+          Shamil R.
         </motion.span>
         <div className='social'>
           <a href='#'>

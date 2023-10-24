f4c6795f57b4ac0df3c464e449dea39c6a5da78f
Shr4528 <Shr4528@gmail.com>
2023-10-24 12:47:45 -0500

    animateservices

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,34 +1,60 @@
+import { useRef } from 'react';
 import './services.scss';
-import { motion } from 'framer-motion';
+import { motion, useInView } from 'framer-motion';
+
+const variants = {
+  initial: {
+    x: -500,
+    y: 100,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 1,
+      staggerChildren: 0.1,
+    },
+  },
+};
 
 const Services = () => {
+  const ref = useRef();
+  const isInView = useInView(ref, { margin: '-100px' });
+
   return (
-    <motion.div className='services'>
-      <motion.div className='textContainer'>
+    <motion.div
+      className='services'
+      variants={variants}
+      initial='initial'
+      ref={ref}
+      animate={isInView && 'animate'}>
+      <motion.div className='textContainer' variants={variants}>
         <p>
           I focus on helping your brand grow <br />
           and move forward
         </p>
         <hr />
       </motion.div>
-      <motion.div className='titleContainer'>
+      <motion.div className='titleContainer' variants={variants}>
         <div className='title'>
           <img src='/people.webp' alt='' />
           <h1>
             {' '}
-            <b>Unique Ideas</b>
+            <motion.b whileHover={{ color: 'orange' }}>Unique Ideas</motion.b>
           </h1>
         </div>
         <div className='title'>
-          <img src='/people.webp' alt='' />
           <h1>
             {' '}
-            <b>For Your</b> Business.
+            <motion.b whileHover={{ color: 'orange' }}>For Your</motion.b>{' '}
+            Business.
           </h1>
           <button>What we do</button>
         </div>
       </motion.div>
-      <motion.div className='listContainer'>
+      <motion.div className='listContainer' variants={variants}>
         <motion.div
           className='box'
           whileHover={{ background: 'lightgray', color: 'black' }}>
